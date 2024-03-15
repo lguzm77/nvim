2f82d9b9fc88cd4a3c73b5eca5b342ad7e78c775
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-03-14 20:34:45 -0400

    None ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,20 +1,20 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				-- You need to install these manually via :Mason
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				require("none-ls.diagnostics.eslint_d"),
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        -- You need to install these manually via :Mason
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        require("none-ls.diagnostics.eslint_d"),
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-		vim.keymap.set("v", "<leader>gf", vim.lsp.buf.format, {}) -- format selection
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    vim.keymap.set("v", "<leader>gf", vim.lsp.buf.format, {}) -- format selection
+  end,
 }
