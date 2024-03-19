0f1e00d07a6b9ee8aaaae0ebffef2e71b6c9a9a1
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-03-19 07:52:11 -0400

    Add :Gbrowse keybinding

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,15 +1,19 @@
 return {
-	--TODO: install octo.nvim
-	{
-		"tpope/vim-fugitive",
-		dependencies = { "tpope/vim-rhubarb" },
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-		end,
-	},
+  --TODO: install octo.nvim
+  {
+    "tpope/vim-fugitive",
+    dependencies = { "tpope/vim-rhubarb" },
+    config = function()
+      vim.keymap.set("n", "<leader>op", ":GBrowse<CR>", {})
+      vim.keymap.set("v", "<leader>op", ":GBrowse<CR>", {})
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+    end,
+  },
 }
