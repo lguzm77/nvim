41a6af9599d6a94adf6157f33a62211985db0539
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-03-20 21:41:29 -0400

    Remap filesystem commands and add relative numbers

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,14 @@
-return  {
+return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-b>', ':Neotree filesystem toggle<CR>')
-    vim.keymap.set('n', '<C-g>', ':Neotree git_status reveal float toggle<CR>') 
-  end
+    vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>")
+    vim.keymap.set("n", "<C-v>", ":Neotree filesystem toggle<CR>")
+    vim.keymap.set("n", "<C-g>", ":Neotree git_status reveal float toggle<CR>")
+  end,
 }
-
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,5 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.g.mapleader = " " --alias for leader
-vim.cmd("cd %:p:h")
-
+vim.cmd("cd %:p:h") -- set current directory as base
+vim.wo.relativenumber = true
