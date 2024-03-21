d1281bdc3ebfbceb794dfe817853781c1cd70fe1
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-03-20 21:27:41 -0400

    Remove redundant neotree keybinding

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,6 @@ return  {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
     vim.keymap.set('n', '<C-b>', ':Neotree filesystem toggle<CR>')
     vim.keymap.set('n', '<C-g>', ':Neotree git_status reveal float toggle<CR>') 
   end
