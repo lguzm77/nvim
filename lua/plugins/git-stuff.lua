ff62f3b49f240589c502e319ead8c313de876e43
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-03-30 09:02:51 -0400

    Add octo.nvim

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,19 +1,30 @@
 return {
-  --TODO: install octo.nvim
-  {
-    "tpope/vim-fugitive",
-    dependencies = { "tpope/vim-rhubarb" },
-    config = function()
-      vim.keymap.set("n", "<leader>op", ":GBrowse<CR>", {})
-      vim.keymap.set("v", "<leader>op", ":GBrowse<CR>", {})
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-    end,
-  },
+	{
+		"pwntester/octo.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup({ enable_builtin = true })
+		end,
+		keys = { { "<leader>O", "<cmd>Octo<cr>", desc = "Octo" } },
+	},
+	{
+		"tpope/vim-fugitive",
+		dependencies = { "tpope/vim-rhubarb" },
+		config = function()
+			vim.keymap.set("n", "<leader>op", ":GBrowse<CR>", {})
+			vim.keymap.set("v", "<leader>op", ":GBrowse<CR>", {})
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+		end,
+	},
 }
