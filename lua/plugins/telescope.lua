3a8ee6ff236b2de3c4e0b682126e7c5ecd5acb0e
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-03-31 21:03:24 -0400

    add hardtime.nvim and remap find files keymapping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return  {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {}) -- fuzzy finder
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- fuzzy finder
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})-- grep
     end
   },
