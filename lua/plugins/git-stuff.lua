398b21dab0a64f543870389323d5a8c0038a78b5
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-03-31 21:07:20 -0400

    change current line blame keybinding

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -24,7 +24,7 @@ return {
 		config = function()
 			require("gitsigns").setup()
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+			vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
 		end,
 	},
 }
