df5beef39851ed36cd3331b2ec65353c83a2c314
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-04-02 08:47:17 -0400

    change relative number coloring

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,13 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin-frappe"
-  end
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({ flavour = "frappe", transparent_background = true })
+
+		vim.cmd.colorscheme("catppuccin")
+		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#F7E7CE", bold = true })
+		vim.api.nvim_set_hl(0, "LineNr", { fg = "#CEB25E", bold = true })
+		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#F7E7CE", bold = true })
+	end,
 }
-
-
