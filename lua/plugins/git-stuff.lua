a17a8e2a24a613ccfa4f3bbbe7cee4318c122e71
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-04-22 08:29:01 -0400

    add lazygit.nvim

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,29 +1,48 @@
 return {
-	{
-		"pwntester/octo.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("octo").setup({ enable_builtin = true })
-		end,
-		keys = { { "<leader>O", "<cmd>Octo<cr>", desc = "Octo" } },
-	},
-	{
-		"tpope/vim-fugitive",
-		dependencies = { "tpope/vim-rhubarb" },
-		config = function()
-			vim.keymap.set({ "n", "v" }, "<leader>op", ":GBrowse<CR>", {})
-		end,
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
-		end,
-	},
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+    },
+  },
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup({ enable_builtin = true })
+    end,
+    keys = { { "<leader>O", "<cmd>Octo<cr>", desc = "Octo" } },
+  },
+  {
+    "tpope/vim-fugitive",
+    dependencies = { "tpope/vim-rhubarb" },
+    config = function()
+      vim.keymap.set({ "n", "v" }, "<leader>op", ":GBrowse<CR>", {})
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
+    end,
+  },
 }
