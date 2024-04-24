b7486fc65b533efe79493bb774835619701e554f
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-04-23 21:34:28 -0400

    Update alpha, update telescope and add vim tmux navigator

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,25 @@
-return  {
-  { 'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {}) -- fuzzy finder
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})-- grep
-    end
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" }) -- fuzzy finder
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" }) -- grep
+    end,
   },
   {
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope-ui-select.nvim",
     config = function()
-      require("telescope").setup {
+      require("telescope").setup({
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
       require("telescope").load_extension("ui-select")
-    end
-  }
+    end,
+  },
 }
-
