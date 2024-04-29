e61cc78f417825410bd1c6a2c5b05903adb442ed
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-04-29 08:42:13 -0400

    detach git-signs to its own file

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -37,12 +37,4 @@ return {
       vim.keymap.set({ "n", "v" }, "<leader>op", ":GBrowse<CR>", {})
     end,
   },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
-    end,
-  },
 }
