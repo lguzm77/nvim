c86089bcc15c3296b4bbbafb729da929e577a3ff
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-04-29 08:45:21 -0400

    enhance tokyonight theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,25 +1,45 @@
 return {
   "folke/tokyonight.nvim",
-  lazy = false,
   priority = 1000,
-  opts = {},
   config = function()
+    local transparent = false -- set to true if you would like to enable transparency
+
+    local bg = "#011628"
+    local bg_dark = "#011423"
+    local bg_highlight = "#143652"
+    local bg_search = "#0A64AC"
+    local bg_visual = "#275378"
+    local fg = "#CBE0F0"
+    local fg_dark = "#B4D0E9"
+    local fg_gutter = "#627E97"
+    local border = "#547998"
+
     require("tokyonight").setup({
-      style = "storm",
-      transparent = false,
+      style = "night",
+      transparent = transparent,
       styles = {
-        -- Style to be applied to different syntax groups
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true, bold = true },
-        keywords = { italic = true, bold = true },
-        functions = { italic = true, bold = true },
+        sidebars = transparent and "transparent" or "dark",
+        floats = transparent and "transparent" or "dark",
       },
+      on_colors = function(colors)
+        colors.bg = bg
+        colors.bg_dark = transparent and colors.none or bg_dark
+        colors.bg_float = transparent and colors.none or bg_dark
+        colors.bg_highlight = bg_highlight
+        colors.bg_popup = bg_dark
+        colors.bg_search = bg_search
+        colors.bg_sidebar = transparent and colors.none or bg_dark
+        colors.bg_statusline = transparent and colors.none or bg_dark
+        colors.bg_visual = bg_visual
+        colors.border = border
+        colors.fg = fg
+        colors.fg_dark = fg_dark
+        colors.fg_float = fg
+        colors.fg_gutter = fg_gutter
+        colors.fg_sidebar = fg_dark
+      end,
     })
 
-    vim.cmd([[colorscheme tokyonight]])
-    local nonHighlightColor = "#A9B1D6"
-    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = nonHighlightColor, bold = true })
-    vim.api.nvim_set_hl(0, "LineNr", { fg = "#FFC777", bold = true })
-    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = nonHighlightColor, bold = true })
+    vim.cmd("colorscheme tokyonight")
   end,
 }
