534a40fa7429be3b62f6f7bcdac4cf7d7f7dbd37
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-05-04 12:55:35 -0400

    fix import bugs

--- init.lua
+++ init.lua
@@ -1,18 +1,2 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require("vim-options")
-require("keymappings")
--- TODO: organize your imports into subfolders
-require("lazy").setup("plugins")
+require("lev.core")
+require("lev.lazy")
--- lua/lev/plugins/lsp/mason.lua
+++ lua/lev/plugins/lsp/mason.lua
@@ -1,44 +1,44 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-        local mason = require("mason")
-        local mason_lspconfig = require("mason-lspconfig")
-        local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-        })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-        mason_lspconfig.setup({
-            auto_install = true,
-    ensure_installed = {
-      "tsserver",
-      "gopls",
-      "omnisharp"
-    }
-        })
+		mason_lspconfig.setup({
+			auto_install = true,
+			ensure_installed = {
+				"tsserver",
+				"gopls",
+				"omnisharp",
+			},
+		})
 
-  -- install linters and formatters
-        mason_tool_installer.setup({
-            ensure_installed = {
-                "prettier",
-                "stylua",
-                "eslint_d",
-                "codespell",
-                "golangci-lint",
-                "gofumpt",
-                "marksman",
-            },
-        })
-    end,
-},
+		-- install linters and formatters
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"eslint_d",
+				"codespell",
+				"golangci-lint",
+				"gofumpt",
+				"marksman",
+			},
+		})
+	end,
+}
