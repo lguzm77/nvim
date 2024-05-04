3bedee86397a7f74d7466f7b541938277db15e45
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-05-04 08:01:42 -0400

    unify mason and mason-lspconfig and add mason auto installer

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,56 +1,76 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = true,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    config = function()
-      require("mason-lspconfig").setup({
-        auto_install = true,
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			local mason = require("mason")
+			local mason_lspconfig = require("mason-lspconfig")
+			local mason_tool_installer = require("mason-tool-installer")
 
-      local lspconfig = require("lspconfig")
+			mason.setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tsserver.setup({ -- you need to run npm install -g typescript typescript-language-server
-        capabilities = capabilities,
-      })
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities,
-      })
+			mason_lspconfig.setup({
+				auto_install = true,
+			})
 
-      local omnisharp_exec_path = vim.fn.stdpath("data") .. "/mason/packages/libexec/OmniSharp.dll"
-      lspconfig.omnisharp.setup({
-        capabilities = capabilities,
-        cmd = { "dotnet", omnisharp_exec_path },
-        settings = {
-          RoslynExtensionsOptions = {
-            EnableAnalyzersSupport = true,
-            AnalyzeOpenDocumentsOnly = true,
-          },
-        },
-      })
+			mason_tool_installer.setup({
+				ensure_installed = {
+					"prettier",
+					"stylua",
+					"eslint_d",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      lspconfig.marksman.setup({ capabilities = capabilities })
+			local lspconfig = require("lspconfig")
 
-      -- TODO: explore more lsp functionalities and add keybindings
-      vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, { desc = "Hover over word for lsp suggestions " })
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "See keyword references" })
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "See available code actions" })
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "rename variable" })
-    end,
-  },
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({ -- you need to run npm install -g typescript typescript-language-server
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+
+			local omnisharp_exec_path = vim.fn.stdpath("data") .. "/mason/packages/libexec/OmniSharp.dll"
+			lspconfig.omnisharp.setup({
+				capabilities = capabilities,
+				cmd = { "dotnet", omnisharp_exec_path },
+				settings = {
+					RoslynExtensionsOptions = {
+						EnableAnalyzersSupport = true,
+						AnalyzeOpenDocumentsOnly = true,
+					},
+				},
+			})
+
+			lspconfig.marksman.setup({ capabilities = capabilities })
+
+			-- TODO: explore more lsp functionalities and add keybindings
+			vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, { desc = "Hover over word for lsp suggestions " })
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "See keyword references" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "See available code actions" })
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "rename variable" })
+		end,
+	},
 }
