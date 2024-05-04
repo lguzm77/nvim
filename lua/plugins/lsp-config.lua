6fe3c3ccde07a919aa3247b6d1a6a871a6902b6f
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-05-04 12:11:04 -0400

    refactor lsp configuration

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -33,54 +33,109 @@ return {
 					"golangci-lint",
 					"gofumpt",
 					"gopls",
-          "lua-language-server",
-          "typescript-language-server",
-          "omnisharp",
-          "marksman",
-          "rust_analyzer"
+					"lua-language-server",
+					"typescript-language-server",
+					"omnisharp",
+					"marksman",
+					"rust-analyzer",
+					"htmx-lsp",
 				},
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
-		lazy = false,
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			{ "antosha417/nvim-lsp-file-operations", config = true },
+			{ "folke/neodev.nvim", opts = {} },
+		},
 		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
+			local capabilities = cmp_nvim_lsp.default_capabilities()
 			local lspconfig = require("lspconfig")
+			local mason_lspconfig = require("mason-lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
+			local keymap = vim.keymap
+
+			-- keymaps
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf, silent = true }
+
+					-- set keybinds
+					opts.desc = "Show LSP references"
+					keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+
+					opts.desc = "Go to declaration"
+					keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+					opts.desc = "Show LSP definitions"
+					keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+					opts.desc = "Show LSP implementations"
+					keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+
+					opts.desc = "Show LSP type definitions"
+					keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+					opts.desc = "See available code actions"
+					keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+					opts.desc = "Smart rename"
+					keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+
+					opts.desc = "Show buffer diagnostics"
+					keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+					opts.desc = "Show line diagnostics"
+					keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+					opts.desc = "Go to previous diagnostic"
+					keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+					opts.desc = "Go to next diagnostic"
+					keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+					opts.desc = "Show documentation for what is under cursor"
+					keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+					opts.desc = "Restart LSP"
+					keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				end,
 			})
-			lspconfig.tsserver.setup({ -- you need to run npm install -g typescript typescript-language-server
-				capabilities = capabilities,
+
+			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
+
+			mason_lspconfig.setup_handlers({
+				-- default handler for installed servers
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+				--specific handlers
+				["omnisharp"] = function()
+					local omnisharp_exec_path = vim.fn.stdpath("data") .. "/mason/packages/libexec/OmniSharp.dll"
+					lspconfig.omnisharp.setup({
+						capabilities = capabilities,
+						cmd = { "dotnet", omnisharp_exec_path },
+						settings = {
+							RoslynExtensionsOptions = {
+								EnableAnalyzersSupport = true,
+								AnalyzeOpenDocumentsOnly = true,
+							},
+						},
+					})
+				end,
 			})
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-			})
-
-			local omnisharp_exec_path = vim.fn.stdpath("data") .. "/mason/packages/libexec/OmniSharp.dll"
-			lspconfig.omnisharp.setup({
-				capabilities = capabilities,
-				cmd = { "dotnet", omnisharp_exec_path },
-				settings = {
-					RoslynExtensionsOptions = {
-						EnableAnalyzersSupport = true,
-						AnalyzeOpenDocumentsOnly = true,
-					},
-				},
-			})
-
-			lspconfig.marksman.setup({ capabilities = capabilities })
-
-			lspconfig.gopls.setup({ capabilities = capabilities })
-
-			-- TODO: explore more lsp functionalities and add keybindings
-			vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, { desc = "Hover over word for lsp suggestions " })
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "See keyword references" })
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "See available code actions" })
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "rename variable" })
 		end,
 	},
 }
