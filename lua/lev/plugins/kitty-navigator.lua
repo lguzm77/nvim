763f7f2e89d43a2e3d4d978fb42827bdde1de5cb
Lev Guzman <levguzman@levguzmaparicio.mynetworksettings.com>
2024-05-12 08:23:45 -0400

    enable kitty-navigator, disable pets.nvim and delete vim tmux nav

--- lua/lev/plugins/kitty-navigator.lua
+++ lua/lev/plugins/kitty-navigator.lua
@@ -1,40 +1,12 @@
 return {
 	"MunsMan/kitty-navigator.nvim",
-  enabled = false,
 	build = { "cp navigate_kitty.py ~/.config/kitty", "cp pass_keys.py ~/.config/kitty" },
-	keys = {
-		{
-			-- C keys don't work here, why?
-			"<leader>h",
-			function()
-				require("kitty-navigator").navigateLeft()
-			end,
-			mode = "n",
-			desc = "Move left a Split",
-		},
-		{
-			"<leader>j",
-			function()
-				require("kitty-navigator").navigateDown()
-			end,
-			mode = "n",
-			desc = "Move down a Split",
-		},
-		{
-			"<leader>k",
-			function()
-				require("kitty-navigator").navigateUp()
-			end,
-			mode = "n",
-			desc = "Move up a Split",
-		},
-		{
-			"<leader>l",
-			function()
-				require("kitty-navigator").navigateRight()
-			end,
-			mode = "n",
-			desc = "Move right a Split",
+	opts = {
+		keybindings = {
+			left = "<C-h>",
+			down = "<C-j>",
+			up = "<C-k>",
+			right = "<C-l>",
 		},
 	},
 }
--- lua/lev/plugins/pets.lua
+++ lua/lev/plugins/pets.lua
@@ -1,5 +1,6 @@
 return {
 	"giusgad/pets.nvim",
+  enable = false,
 	dependencies = { "MunifTanjim/nui.nvim", "giusgad/hologram.nvim" },
 	config = function()
 		require("pets").setup({
