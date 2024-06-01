fe9452d42584cf9dea10456f99585f1ed9d55053
Lev Guzman <levguzman@Levs-MacBook-Pro.local>
2024-06-01 17:47:03 -0400

    add find keymap and fuzzy find in current buffer

--- lua/lev/plugins/telescope.lua
+++ lua/lev/plugins/telescope.lua
@@ -62,17 +62,22 @@ return {
 			telescope.load_extension("ui-select")
 
 			local keymap = vim.keymap
+			local builtin = require("telescope.builtin")
+      local themes = require("telescope.themes")
 
 			keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 			keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 			keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-			keymap.set(
-				"n",
-				"<leader>fw",
-				"<cmd>Telescope grep_string<cr>",
-				{ desc = "Find word under cursor in cwd" }
-			)
+			keymap.set("n", "<leader>fw", "<cmd>Telescope grep_string<cr>", { desc = "Find word under cursor in cwd" })
 			keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+			keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })
+			keymap.set("n", "<leader>/", function()
+				builtin.current_buffer_fuzzy_find(themes.get_dropdown({te
+					winblend = 10,
+					previewer = false,
+					layout_config = { width = 0.7 },
+				}))
+			end, { desc = "[/] Fuzzy search in current buffer" })
 		end,
 	},
 	{
