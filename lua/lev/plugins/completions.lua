35fcd7f8aecbf94bff77c1c4697a7de9ea01ef30
Lev Guzman <levguzman@Levs-MacBook-Pro.local>
2024-06-08 07:58:08 -0600

    Change doc scrolling keybindings

--- lua/lev/plugins/completions.lua
+++ lua/lev/plugins/completions.lua
@@ -45,8 +45,8 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-j>"] = cmp.mapping.scroll_docs(-4),
-					["<C-k>"] = cmp.mapping.scroll_docs(4),
+					["<C-i>"] = cmp.mapping.scroll_docs(-4),
+					["<C-o>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
